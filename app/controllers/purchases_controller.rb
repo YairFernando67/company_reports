adaec9d96e7e8a977236716121db988a84a81fd7
Yair <yair.facio11@gmail.com>
2020-10-11 16:13:05 -0500

    Set up the purchases controller to respond to pdf format

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,7 +2,33 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!
 
   def generate_report
-    result = PurchaseReportService.new(params, current_user).call
-    render json: result
+    @report = PurchaseReportService.new(params, current_user).call
+    # @report.get_header
+    # binding.pry
+    # render json: result
+    # binding.pry#e91e63
+    # binding.pry
+    respond_to do |format|
+      format.html
+      format.js
+      format.pdf do
+        render pdf: "Sales Report",
+        # javascript_delay: 1000,
+        # header: {
+        #   html: {
+        #     template: 'purchases/header.html.slim',
+        #     locals: { user: @report[:user] },
+        #   }
+        # },
+        # show_as_html: true,
+        page_size: 'A4',
+        # window_status: "FLAG_FOR_PDF",
+        template: "purchases/generate_report.html.slim",
+        layout: "pdf.html",
+        lowquality: true,
+        zoom: 1,
+        dpi: 75
+      end
+    end
   end
 end
