0487242e43e998e1a93a4444b23261d62122b4b3
yairfacio <yair.facio11@gmail.com>
2021-01-15 20:56:57 -0600

    Add views for decorator

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   
-  
+
   root to: 'home#index'
   get '/sale_report', action: :generate_report, controller: :sales
   devise_for :users
 
   # Design Patterns Routes
   scope :design_patterns, module: "design_patterns" do
-    get 'builder/index'
+    get 'builder', action: :index, controller: :builder
+    get 'decorator', action: :index, controller: :decorator
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
