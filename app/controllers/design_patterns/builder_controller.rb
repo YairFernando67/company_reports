8c8334d8bb7172fd6a9c00979fca7088cfb7b377
yairfacio <yair.facio11@gmail.com>
2021-02-01 20:27:43 -0600

    Modify routes for builder

--- app/controllers/design_patterns/builder_controller.rb
+++ app/controllers/design_patterns/builder_controller.rb
@@ -1,6 +1,34 @@
 module DesignPatterns
   class BuilderController < ApplicationController
+    before_action :authenticate_user!
+
     def index
+    end
+
+
+    def generate_report
+      @report = SaleReportService.new(params, current_user).call
+      @company_earnings = @report.ui.data[:charts][:company_earnings]
+      # binding.pry
+      # respond_to do |format|
+      #   format.html
+      #   format.js
+      #   format.pdf do
+      #     render pdf: "Sales Report",
+      #     header: {
+      #       html: {
+      #         template: 'design_patterns/builder/header.html.slim',
+      #         locals: { user: @report },
+      #       }
+      #     },
+      #     page_size: 'A4',
+      #     template: "design_patterns/builder/generate_report.html.slim",
+      #     layout: "pdf.html",
+      #     lowquality: true,
+      #     zoom: 1,
+      #     dpi: 75
+      #   end
+      # end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
 
   get 'factory/index'
   root to: 'home#index'
-  get '/sale_report', action: :generate_report, controller: :sales
+  # get '/sale_report', action: :generate_report, controller: :sales
   devise_for :users
 
   # Design Patterns Routes
   scope :design_patterns, module: "design_patterns" do
-    get 'builder', action: :index, controller: :builder
+    scope :builder do
+      get '/', action: :index, controller: :builder
+      get '/sale_report', action: :generate_report, controller: :builder
+    end
     get 'decorator', action: :index, controller: :decorator
     get 'factory', action: :index, controller: :factory
   end
