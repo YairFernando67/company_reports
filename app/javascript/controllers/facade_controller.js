bec3dce35d7ecb1fda16a2fcdcdf4a420ed61691
yairfacio <yair.facio11@gmail.com>
2021-04-11 01:01:13 -0500

    Add facade js controller

--- app/javascript/controllers/facade_controller.js
+++ app/javascript/controllers/facade_controller.js
@@ -2,16 +2,45 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
 
-  static targets = ["form"]
+  static targets = ["form", "summary", "description"]
 
   initialize() {
     // console.log(this.formTarget)
     // console.log("facade")
   }
 
+  createCalendar() {
+    let summary = $(this.summaryTarget).val()
+    let description = $(this.descriptionTarget).val()
+    let url = $(this.formTarget).attr("action")
+    let serializeForm = $(this.formTarget).serialize()
+    console.log(summary)
+    console.log(description)
+    console.log(url)
+    console.log(serializeForm)
+    $.ajax({
+      type: "POST",
+      url:url,
+      data: {summary, description},
+      success: function(r){
+        console.log("Response", r)
+        console.log(this)
+        window.current_this = this
+        hideCalendarFrom()
+      },
+      error: function(xhr, textStatus, errorThrown) {
+        console.log(xhr.responseJSON)
+      }
+    })
+  }
 
   showCalendarFrom() {
     console.log("Show form")
     $(this.formTarget).removeClass("hidden")
+  }
+
+  hideCalendarFrom() {
+    console.log("Hide form")
+    $(".calendar-form").addClass("hidden")
   }
 }
