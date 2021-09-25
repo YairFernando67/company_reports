cc772844a50928c3811c130f3ca2e234494eadfe
yairfacio <yair.facio11@gmail.com>
2021-09-25 15:56:41 -0500

    Add rails credential for callback endpoint

--- app/controllers/design_patterns/gmail_calendars_controller.rb
+++ app/controllers/design_patterns/gmail_calendars_controller.rb
@@ -4,7 +4,8 @@ module DesignPatterns
   class GmailCalendarsController < ApplicationController
     before_action :get_instance, only: %i[create_calendar delete_calendar gmail_calendars events]
     before_action :valid_token, only: %i[create_calendar delete_calendar gmail_calendars events]
-    CALLBACK_URL = "http://localhost:3000/design_patterns/facade/calendar_authorized"
+    CALLBACK_URL = "#{Rails.application.credentials[Rails.env.to_sym].dig(:callback_url)}/design_patterns/facade/calendar_authorized"
+
 
     def create_calendar
       client.create_calendar(calendar_params)
